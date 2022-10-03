55458e3fc6e63b86a5994ca06cf4c1a6c8cd9f33
JoriamAvans <j.vanslooten@student.avans.nl>
2022-10-03 10:32:45 +0200

    Finalized Sprint_1 (No Feedback)
    Updated text parsing to csv. 
    Put the program in a loop to encourage user friendliness. 
    Made the file more organized with whitespace.

--- main.py
+++ main.py
@@ -16,43 +16,57 @@ Github:https://github.com/JoriamVS/Stationszuil
 '''
 
 import random
+import csv
 from datetime import datetime
 random.seed()
 
+
 def startupCLI():
+
     print(firstprint)
     print("\n\n\n")
 
 
 def userInput():
+
     userName = input("Voer uw naam in. In het geval dat u dit veld leeg laat blijft u anoniem: ")
+
     if userName is "":
         userName = "Anoniem"
     userMessage = input("Voer uw bericht in. Deze mag maximaal 140 karakters bevatten: ")
+
     if len(userMessage) > 140:
         print("Uw bericht was te lang, probeer het nog een keer.\n")
-        userInput() # This might need a fix later on. When a user tries a lot of times the stack can fill up.
+        userInput() # This might need a fix later on. When a user tries a lot of times, the stack can fill up.
+
     userInputList = [userMessage, userName]
     return userInputList
 
 
 def parseData(userList):
+
     stationList = ["Amersfoort Centraal", "Utrecht Centraal", "'S-Hertogenbosch"]
-    randStation = stationList[random.randint(0,2)]
+    randStation = stationList[random.randint(0, 2)]
+
     currentDateTime = datetime.now()
     msgTime = currentDateTime.strftime("%d/%m/%Y %H:%M:%S")
     filename = currentDateTime.strftime("%d%m%Y_%H%M%S")
-    # Write to text file
-    with open(f'messages/{filename}', 'w') as myFile:
-        myFile.write(userList[0])  # Message
-        myFile.write("\n")
-        myFile.write(msgTime)  # Date and time
-        myFile.write("\n")
-        myFile.write(userList[1])  # Username
-        myFile.write("\n")
-        myFile.write(randStation)  # Random train station
+    
+    # Write to csv file
+    with open(f'messages/{filename}.csv', 'w') as myFile:
+
+        columns = ["Message", "DateTime", "Username", "Station"]
+        rows = [userList[0], msgTime, userList[1], randStation]  # Message, Date and time, Username, Random train station
+
+        writer = csv.writer(myFile)
+        writer.writerow(columns)
+        writer.writerow(rows)
+
+        print("\n")
 
 
 startupCLI()
-message = userInput()
-parseData(message)
+
+while True:
+    message = userInput()
+    parseData(message)
